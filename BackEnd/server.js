c2e63422f2b1266f96c19f00cf9d8aae7add1094
haftamudesta <desta451616@hotmail.com>
2024-07-05 20:24:53 -0800

    Add back end

---
+++ BackEnd/server.js
@@ -0,0 +1,30 @@
+const express= require("express");
+const mongoose =require("mongoose");
+const Book =require("./modules/BookModule");
+const DBConnection =require("./DBConnection");
+
+const BooksRoute=require("./routes/BooksRoute")
+const cors=require('cors')
+const dotenv =require("dotenv");
+dotenv.config({path:'./config.env'});//this should be before express
+
+const server=express();
+const PORT=process.env.PORT || 5000;
+
+server.use(express.json());
+//CORS=cross origin resource sharing
+ server.use(cors())
+//     origin:'http://localhost:3000',
+//     methods:['POST','GET','PUT','DELETE'],
+//     allowedHeaders:['Content-Type']
+
+// }));
+server.use('/books',BooksRoute);
+
+DBConnection();
+server.listen(PORT,()=>{
+    console.log(`server is running at port ${PORT}`);
+})
+
+
+
